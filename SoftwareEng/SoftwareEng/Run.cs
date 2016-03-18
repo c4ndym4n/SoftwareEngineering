352b030fb5b0c754fca36da1fb14a7a198abb8ac
c4ndym4n <c4ndym4n2.0@outlook.com>
2016-03-19 00:53:21 +0100

    Add all Files

---
+++ SoftwareEng/SoftwareEng/Run.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareEng
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            // Here starts everything
+            Console.WriteLine("ALOO Project");
+        }
+    }
+}
